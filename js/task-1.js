dbb5ae0e070b62c125d0d0db3676aec9fab9f171
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-11-05 22:53:36 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,11 @@
  const name = '«Генератор защитного поля»'; 
  let price = 1000; 
+ const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; 
  
- const firstMessage = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`; 
+ console.log(firstMessage);
  
  price = 2000;
-
- const secondMessage = `Выбран «Генератор защитного поля», цена за штуку 2000 кредитов`; 
-
-console.log(firstMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+ const secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; 
 
 console.log(secondMessage);
-// 'Выбран «Генератор защитного поля», цена за штуку 2000 кредитов'
 
