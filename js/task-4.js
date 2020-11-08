2d9292519d45ddc2a12c1f04d3a461a8c8e83a01
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-11-08 23:25:30 +0200

    task_5

--- js/task-4.js
+++ js/task-4.js
@@ -1,27 +1,27 @@
 'use strict'
 
-let orderPieces = prompt('Enter the number of droids');
+// let orderPieces = prompt('Enter the number of droids');
 
-const credits = 23580;
-const pricePerDroid = 3000;
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const ACCESS_DENIED = 'Недостаточно средств на счету!';
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
-let totalPrice = orderPieces * pricePerDroid; 
-let balanceCredit = credits - totalPrice; 
-let message;
+// let totalPrice = orderPieces * pricePerDroid; 
+// let balanceCredit = credits - totalPrice; 
+// let message;
 
-if (orderPieces === null) {
-    message = CANCELED_BY_USER;
-} else {
-    if (totalPrice > credits) {
-        message = ACCESS_DENIED;
-    } else {
-        message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
-    }
-}
+// if (orderPieces === null) {
+//     message = CANCELED_BY_USER;
+// } else {
+//     if (totalPrice > credits) {
+//         message = ACCESS_DENIED;
+//     } else {
+//         message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+//     }
+// }
 
-console.log(message);
+// console.log(message);
 
 
 // Напиши скрипт который:
